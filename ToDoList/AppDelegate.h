e84af2e2ca216cad206490d58a862c726395e80a
Lucas Churchill <lucas@agtlucas.com>
2014-08-19 14:56:31 -0300

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Lucas Churchill on 8/19/14.
+//  Copyright (c) 2014 awlka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoListTests/ToDoListTests.m
@@ -0,0 +1,34 @@
+//
+//  ToDoListTests.m
+//  ToDoListTests
+//
+//  Created by Lucas Churchill on 8/19/14.
+//  Copyright (c) 2014 awlka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToDoListTests : XCTestCase
+
+@end
+
+@implementation ToDoListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
